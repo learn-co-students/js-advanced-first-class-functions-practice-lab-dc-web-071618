5ab3ac85a646bbc9759f60a95bc8b8c65e776591
aubreeabril <aubree.abril@gmail.com>
2018-08-27 12:10:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function logDriverNames(driversArray) {
+  driversArray.forEach(function (driver) {
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(driversArray, location) {
+  driversArray.forEach(function (driver) {
+    if (location === driver.hometown) {
+      console.log(driver.name);
+    }
+  });
+}
+
+function driversByRevenue(driversArray) {
+  return driversArray.slice().sort(function (driv1, driv2) {
+    return driv1.revenue - driv2.revenue;
+  });
+}
+
+function driversByName(driversArray) {
+  return driversArray.slice().sort(function (driv1, driv2) {
+    return driv1.name.localeCompare(driv2.name);
+  });
+}
+
+function totalRevenue(driversArray) {
+  return driversArray.reduce(function (sum, driver) {
+    return driver.revenue + sum;
+  }, 0);
+}
+
+function averageRevenue(driversArray) {
+  return totalRevenue(driversArray) / driversArray.length;
+}
